13edb14953b5f0a7f3f5b7342e64033d292ef63a
hl.lee <hl.lee@quintet.co.kr>
2024-06-09 17:29:25 +0900

    ✨캘린더 빈 페이지 추가

---
+++ lib/calendarView.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CalendarView extends StatelessWidget {
+  const CalendarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              context.go('/');
+            },
+            child: Text("홈 화면으로 이동")
+        ),
+      )
+    );
+  }
+}
--- lib/customRouter.dart
+++ lib/customRouter.dart
@@ -2,13 +2,15 @@ import 'package:hatssue/main.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:hatssue/sampleView.dart';
+import 'package:hatssue/calendarView.dart';
 
 
 class CustomRouter {
   static GoRouter router = GoRouter(
       routes: [
         GoRoute(path: "/", builder: (context, state) => MyHomePage(title: "제목")),
-        GoRoute(path: "/sampleView", builder: (context, state) => SampleView())
-  ]
+        GoRoute(path: "/sampleView", builder: (context, state) => SampleView()),
+        GoRoute(path: "/calendarView", builder: (context, state) => CalendarView()),
+    ]
   );
 }
