6fda55d81694daf8f3c724d4871b515873c4a447
hyelee <hyeleex2@gmail.com>
2024-07-12 16:39:01 +0900

    fix: theme 컬러 변경

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -9,7 +9,7 @@ class DarkTheme implements AppTheme {
     surface: Palette.primary02,
     // background: background,
     text: Palette.grey01,
-    subtext: Palette.primary02,
+    subtext: Palette.grey04, // 임시
     // toastContainer: toastContainer,
     // onToastContainer: onToastContainer,
     // hint: hint,
