ff6fba084540692ca2aa42036656f8aaeec1a3a0
hyeleex2 <hyeleex2@gmail.com>
2024-07-16 16:09:22 +0900

    fix: 캘린더 > 시작 날짜 선택시 종료 날짜 셋팅 로직 추가

--- lib/features/challenge/presentaion/screens/challenge_period_view.dart
+++ lib/features/challenge/presentaion/screens/challenge_period_view.dart
@@ -5,11 +5,6 @@ import 'package:hatssue/service/theme/theme_service.dart';
 import 'package:hatssue/utils/intl_helper.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-// class 멤버 변수로 선언되면 안됨.. 왜지 ..
-final today = DateTime.now();
-final _firstDay = DateTime.now();
-final _lastDay = DateTime(today.year, today.month + 1, today.day);
-
 class ChallengePeriodView extends ConsumerStatefulWidget {
   const ChallengePeriodView({super.key});
 
@@ -24,8 +19,14 @@ class _ChallengePeriodViewState extends ConsumerState<ChallengePeriodView> {
   DateTime? _selectedDay;
   DateTime? _rangeStart; // 오늘 부터
   DateTime? _rangeEnd; // 오늘 부터 + 시작 날짜로부터 100일 이내
-
   String? rangeDiff = '';
+
+  final int durationDays = 100;
+
+  late DateTime _firstDay = DateTime.now();
+  late DateTime _lastDay = _firstDay.add(
+    Duration(days: durationDays),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -69,8 +70,6 @@ class _ChallengePeriodViewState extends ConsumerState<ChallengePeriodView> {
                     return isSameDay(_selectedDay, day);
                   },
                   onDaySelected: (selectedDay, focusedDay) {
-                    // DateTime newLastDay =
-                    //     selectedDay.add(const Duration(days: 7));
                     setState(() {
                       _selectedDay = selectedDay;
                       _focusedDay =
@@ -78,19 +77,23 @@ class _ChallengePeriodViewState extends ConsumerState<ChallengePeriodView> {
                     });
                   },
                   onRangeSelected: (start, end, focusedDay) {
-                    // 차이 구하기
-                    // if (end != null && start != null) {
-                    //   int dayDiff = end.difference(start).inDays;
-                    //   print('dayDiff : $dayDiff');
-                    // }
-                    // 현재 선택한 날짜에 따라 마지막으로 선택 가능한 날짜 변경하기
-
                     setState(() {
                       _selectedDay = null;
                       _focusedDay = focusedDay;
                       _rangeStart = start;
                       _rangeEnd = end;
                       _rangeSelectionMode = RangeSelectionMode.toggledOn;
+                      if (start != null && end == null) {
+                        _firstDay = start;
+                        // 시작 날짜만 존재
+                        // 시작 날짜에 따라 마지막으로 선택 가능한 날짜 변경하기
+                        DateTime newLastDay = start.add(
+                          Duration(
+                            days: durationDays,
+                          ),
+                        );
+                        _lastDay = newLastDay;
+                      }
                     });
                   },
 
